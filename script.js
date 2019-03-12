709f06f86a371bf1ce4c42b1b9c699692b6c3c4a
sAMYRA <samylopes28@gmail.com>
2019-03-12 15:11:14 -0300

    Salvar o começo

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-function transformarX(x) {
-    x.classList.toggle("alterar");
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+const navMenuHamburguer = document.querySelector('.nav-menu-hamburguer');
+
+const atualizarMenu = function () {
+  menuHamburguer.classList.toggle('ativo');
+  navMenuHamburguer.classList.toggle('visivel');
 }
+
+menuHamburguer.onclick = atualizarMenu;
