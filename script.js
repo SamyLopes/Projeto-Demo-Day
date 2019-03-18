c5da3a0eb26786278769f637845a86630878032d
Bianca Lustosa <biafelix08@gmail.com>
2019-03-17 22:45:39 -0300

    Funcionar-noticia

--- script.js
+++ script.js
@@ -10,40 +10,40 @@ menuHamburguer.onclick = atualizarMenu;
 
 
 
-img = new Array('1', '2', '3');
+// img = new Array('1', '2', '3');
 
-indice = 0;
+// indice = 0;
 
-// setInterval("mudaImg()", 5000);
+// // setInterval("mudaImg()", 5000);
 
-function mudaImg(i) {
+// function mudaImg(i) {
 
-  if (i == 0 || i == 1 || i == 2 || i == 3) {
+//   if (i == 0 || i == 1 || i == 2 || i == 3) {
 
-    indice = i;
+//     indice = i;
 
-  } else {
+//   } else {
 
-    if (indice == img.length - 1) {
+//     if (indice == img.length - 1) {
 
-      indice = 0;
+//       indice = 0;
 
-    } else {
+//     } else {
 
-      indice++;
+//       indice++;
 
-    }
+//     }
 
-  }
+//   }
 
-  document.getElementById("banner_img_1").setAttribute("class", "");
-  document.getElementById("banner_img_2").setAttribute("class", "");
-  document.getElementById("banner_img_3").setAttribute("class", "");
-  document.getElementById("banner_img_" + img[indice]).setAttribute("class", "hover");
+//   document.getElementById("banner_img_1").setAttribute("class", "");
+//   document.getElementById("banner_img_2").setAttribute("class", "");
+//   document.getElementById("banner_img_3").setAttribute("class", "");
+//   document.getElementById("banner_img_" + img[indice]).setAttribute("class", "hover");
 
-  document.getElementById("banner_img").innerHTML = "<img src='banner_img/" + img[indice] + ".jpg' width='900' height='300' border='0' alt='Banner'>";
+//   document.getElementById("banner_img").innerHTML = "<img src='banner_img/" + img[indice] + ".jpg' width='900' height='300' border='0' alt='Banner'>";
 
-}
+// }
 
 // const noticia = document.querySelector('banner_img')
 // const mudar = document.querySelector('botoes')
